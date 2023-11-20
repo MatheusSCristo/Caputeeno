c1c19cf408245f43a1437dc2bea4a1de3ecb9c66
Matheus <cristo.matheus@outlook.com>
2023-11-20 19:33:04 -0300

    react config changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  HashRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Product from "./Pages/Product.jsx"
-import Cart from "./Pages/Cart.jsx"
-import Home from './Pages/Home.jsx';
-import ProductProvider from './context/products.jsx';
-import CartProductsProvider from './context/cartproducts.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/product/:id",
-        element: <Product />
-      },
-      {
-        path: "/cart",
-        element: <Cart />
-      },
-      {
-        path: "/",
-        element: <Home />
-      },
-    ]
-  },
-]);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import Home from './Pages/Home';
+import ProductProvider from './context/products';
+import CartProductsProvider from './context/cartproducts';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <React.StrictMode>
-      <CartProductsProvider>
-        <ProductProvider>
-          <RouterProvider router={router} />
-        </ProductProvider>
-      </CartProductsProvider>
-  </React.StrictMode>
-)
+  <HashRouter>
+    <CartProductsProvider>
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </ProductProvider>
+    </CartProductsProvider>
+  </HashRouter>
+);
